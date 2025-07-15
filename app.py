f4eacf485ef1c6193152976eeceb8a9ba47bdf30
Andrian prayugo <andrianprayugo74@gmail.com>
2025-07-15 16:01:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.markdown(
     <style>
     .stApp {
         background-image: linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)),
-                          url("bg.jpg");
+                          url("https://i.imgur.com/O1w6V2n.jpeg");
         background-size: cover;
         background-attachment: fixed;
         background-position: center;
