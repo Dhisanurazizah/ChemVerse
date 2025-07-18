b6fec3fef953131cf80793f2502fcf04548e0bb7
Andrian prayugo <andrianprayugo74@gmail.com>
2025-07-18 17:36:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,11 @@ st.markdown("""
         color: black !important;
         font-weight: bold;
     }
+
+    /* === Ikon Share di pojok kanan atas jadi putih === */
+    button[title="Share this app"] svg {
+        fill: white !important;
+    }
     </style>
 """, unsafe_allow_html=True)
 
